8fcbe6d96e5a62edbbe970d3c22e985ea3ef9d1b
XSDStitch <DavidJager@gmx.net>
2020-03-25 13:23:32 +0100

    Update Placeholder.java

--- ProjektKlausurgenerator/src/de/arbeitsagentur/ProjektKlausurgenerator/Placeholder.java
+++ ProjektKlausurgenerator/src/de/arbeitsagentur/ProjektKlausurgenerator/Placeholder.java
@@ -1,13 +1,10 @@
 package de.arbeitsagentur.ProjektKlausurgenerator;
 
-import test.testPDF;
-
 public class Placeholder {
 
 	public static void main(String[] args) {
 		System.out.println("Placeholder");
 		
-		new testPDF().testKlausur();
 	}
 
 }
